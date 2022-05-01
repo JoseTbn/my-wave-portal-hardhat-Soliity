0d72086b0c9b8489eaba2d4e170d727bff9e89cb
JoseTbn <josebatumeni@gmail.com>
2022-05-01 16:18:56 +0200

    first commit

---
+++ contracts/Waveportal.sol
@@ -0,0 +1,12 @@
+// SPDX-Lincese-Indentifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract WavePortal {
+constructor() {
+console.log("Soy un contracto, y soy intelligentisimo");
+  }
+}
