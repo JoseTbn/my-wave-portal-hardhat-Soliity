80886d1a941f20fe3eb7b9bc70e687cac4fd1370
JoseTbn <josebatumeni@gmail.com>
2022-05-01 18:42:02 +0200

    c

--- contracts/Waveportal.sol
+++ contracts/Waveportal.sol
@@ -6,7 +6,21 @@ import "hardhat/console.sol";
 
 
 contract WavePortal {
+uint256 totalWaves;
+
 constructor() {
 console.log("Soy un contracto, y soy intelligentisimo");
   }
+
+ function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+
 }
